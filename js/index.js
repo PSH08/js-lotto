37b6fb068dfb0b9f0d8afa07544068aed0957b08
lolu1023 <soowkw@naver.com>
2021-12-10 02:00:15 +0900

    수정1

--- js/index.js
+++ js/index.js
@@ -1,15 +1,41 @@
-const btn_start = document.querySelector('.btn .btn1');
-const result = document.querySelector('.result');
-btn_start.addEventListener('click',function () {
-    let lotto = [];
-    for(let i = 0; i < 6; i++) {
-        let rand = Math.floor(Math.random()* 60) + 1;
-        if(lotto.indexOf(rand) === -1){
-            lotto.push(rand)
-        }else {
-            i--
+const btn_start = document.querySelectorAll('.btn_list li');
+const btn_list = document.querySelectorAll('.btn_list');
+for(let i = 0; i < btn_list.length; i++) {
+    btn_list[i].addEventListener('click',(e) => {
+        e.preventDefault();
+        for(let j = 0; j < btn_list.length; j++) {
+            btn_list[j].classList.add('active')
         }
-        console.log(rand)
-    }
-    result.innerHTML(rand)
-});
+        btn_list[i].classList.remove('active')
+    });
+}
+
+for(let i = 0; i < btn_start.length; i++) {
+    btn_start[i].addEventListener('click',function () {
+        let lotto = [];
+        for(let i = 0; i < 6; i++) {
+            let rand = Math.floor(Math.random()* 60) + 1;
+            if(lotto.indexOf(rand) === -1){
+                lotto.push(rand)
+            }else {
+                i--
+            }
+            console.log(rand)
+        }
+    })
+}
+
+
+
+
+
+// let lotto = [];
+// for(let i = 0; i < 6; i++) {
+//     let rand = Math.floor(Math.random()* 60) + 1;
+//     if(lotto.indexOf(rand) === -1){
+//         lotto.push(rand)
+//     }else {
+//         i--
+//     }
+//     console.log(rand)
+// }
