94046c4c345f1e0318ecfae5b6ef09a97e80e600
lolu1023 <soowkw@naver.com>
2021-12-13 19:27:58 +0900

    추가3

--- js/index.js
+++ js/index.js
@@ -1,39 +1,47 @@
 const btn_start = document.querySelectorAll('.btn_list li');
-const btn_list = document.querySelectorAll('.btn_list');
+// const btn_list = document.querySelectorAll('.btn_list');
+const btn_pick = document.querySelector('.lotto_pick');
+const btn_save = document.querySelector(".btn_save")
 const result = document.querySelector('.result');
-const lotto_save = document.querySelector('.lotto_save')
+const lotto_save = document.querySelector('.lotto_save');
 const save = document.querySelector('.btn_save');
-for(let i = 0; i < btn_list.length; i++) {
-    btn_list[i].addEventListener('click',(e) => {
-        e.preventDefault();
-        for(let j = 0; j < btn_list.length; j++) {
-            btn_list[j].classList.add('active')
-        }
-        btn_list[i].classList.remove('active')
-    });
-}
+let globalLotto = []; 
 
-for(let i = 0; i < btn_start.length; i++) {
-    btn_start[i].addEventListener('click',function () {
-        let lotto = [];
-        for(let i = 0; i < 6; i++) {
-            let rand = Math.floor(Math.random()* 60) + 1;
-            if(lotto.indexOf(rand) === -1){
-                lotto.push(rand)
-            }else {
-                i--
-            }
-            console.log(rand)
-        }
-    })
-    //클릭시 result에 나타남
-    result.append(lotto)
-}
 
-save.addEventListener('click',function () {
-    //로또세이브에 나타남
-    // result.value='';
-});
+btn_pick.addEventListener('click',function () {
+    result.innerHTML="";
+    const lotto = [];
+    for(let i = 0; i < 6; i++) {
+        const rand = Math.floor(Math.random()* 60) + 1;
+        if(lotto.indexOf(rand) === -1){
+            lotto.push(rand)
+        }else {
+            i--
+        }
+    }
+    lotto.sort((a,b)=> a-b)
+    globalLotto = lotto;
+    // result.append(lotto)
+    for(let i = 0; i < lotto.length; i++){
+        const li = document.createElement("li")
+        li.innerHTML=lotto[i];
+        result.append(li)
+    }
+})
+
+
+btn_save.addEventListener('click',function () {
+    console.log('before')
+    // lotto_save.innerHTML="";
+    if(globalLotto.length === 0) return;
+    console.log("after")
+    globalLotto.sort((a,b)=> a-b)
+    for(let i = 0; i < globalLotto.length; i++){
+        const li = document.createElement("li")
+        li.innerHTML=globalLotto[i];
+        lotto_save.append(li)
+    }
+})
 
 
 
